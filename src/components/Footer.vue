5271c23ed1bcf18463fed0ac33a75398f47b32d1
justin <lungjyt@hva.nl>
2021-06-21 20:57:27 +0200

    footer
    footer design added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,42 @@
 <template>
-  <footer class="footer">
-    <div class="footer-container-1">
-      <h3>Portfolio</h3>
+  <footer>
+    <div class="footer-wrapper">
+      <div class="footer-logo-container">
+        <h3 class="footer-logo">Portfolio</h3>
+      </div>
+      <div class="footer-menu-container">
+        <ul>
+          <h4 class="footer-title">Menu</h4>
+          <li><a href="">Work</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Services</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </div>
+      <div class="footer-social-container">
+        <ul>
+          <h4 class="footer-title">Social</h4>
+          <li>
+            <a href="https://www.instagram.com/justinlung_/" target="_blank"
+              >Instagram</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/justin-lung-1a0753127/?originalSubdomain=nl"
+              target="_blank"
+              >LinkedIn</a
+            >
+          </li>
+          <li>
+            <a href="https://github.com/JustinLung" target="_blank">Github</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="footer-container-2">
-      <ul>
-        <h3>Socials</h3>
-        <li><a href="">fb</a></li>
-        <li><a href="">ig</a></li>
-        <li><a href="">ln</a></li>
-        <li><a href="">gh</a></li>
-      </ul>
+    <div class="footer-copyright-wrapper">
+      <p>©Justin Lung 2021</p>
     </div>
-    <div class="footer-container-3"></div>
-    <div class="footer-container-4"></div>
   </footer>
 </template>
 <style lang="scss">
@@ -21,8 +44,67 @@
 @import "@/scss/abstracts/_mixins.scss";
 @import "@/scss/base/_global.scss";
 @import "@/scss/base/_typography.scss";
-.footer {
-  background: black;
-  height: 100%;
+footer {
+  background-color: transparent;
+  width: 100%;
+  height: auto;
+
+  .footer-wrapper {
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 750px;
+
+    .footer-logo-container {
+      display: flex;
+      align-items: center;
+      margin-right: 2em;
+    }
+
+    .footer-menu-container {
+      margin: 2em;
+    }
+
+    .footer-social-container {
+      margin: 2em;
+    }
+  }
+
+  .footer-copyright-wrapper {
+    max-width: 750px;
+    padding-bottom: 2em;
+    margin: 0 auto;
+
+    color: #fff;
+    border: 0;
+    border-top: 0.1em;
+    border-style: solid;
+    padding: 2em;
+    padding-bottom: 2em;
+  }
+}
+
+@media (max-width: 768px) {
+  footer {
+    .footer-wrapper {
+      flex-direction: column;
+      padding: 2em;
+
+      .footer-logo-container {
+        margin: 0;
+        margin-top: 2em;
+      }
+
+      .footer-menu-container {
+        margin: 0;
+        margin-top: 2em;
+      }
+
+      .footer-social-container {
+        margin: 0;
+        margin-top: 2em;
+      }
+    }
+  }
 }
 </style>
